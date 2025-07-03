741bb5cb47e7c19ff6423c9643faf4a06ef721f6
miasriko <info@neuf.design>
2025-07-03 22:15:53 +0900

    gsap追加

--- assets/js/component/_gsapAnimation.js
+++ assets/js/component/_gsapAnimation.js
@@ -1,17 +1,33 @@
 export function gsapAnimation() {
   gsap.registerPlugin(ScrollTrigger);
 
-document.querySelectorAll('.js-rotate').forEach((el) => {
-  const rotationAmount = gsap.utils.random(10, 180);
-  gsap.to(el, {
-    rotation: rotationAmount,
-    ease: 'none',
+  document.querySelectorAll('.js-rotate').forEach((el) => {
+    const rotationAmount = gsap.utils.random(10, 180);
+    gsap.to(el, {
+      rotation: rotationAmount,
+      ease: 'none',
+      scrollTrigger: {
+        trigger: el,
+        start: 'top bottom',   // 要素の上端が画面下に入ったら開始
+        end: 'bottom top',     // 要素の下端が画面上に出たら終了
+        scrub: 2            // スクロールと連動
+      }
+    });
+  });
+
+  gsap.to(".js-zoom-target", {
+    scale: 1.13,
     scrollTrigger: {
-      trigger: el,
-      start: 'top bottom',   // 要素の上端が画面下に入ったら開始
-      end: 'bottom top',     // 要素の下端が画面上に出たら終了
-      scrub: 2            // スクロールと連動
+      trigger: ".js-zoom-section",
+      start: "top top",
+      end: "+=100%",
+      scrub: 1,
+      pin: true,
     }
   });
-});
+
+  // リサイズ時にScrollTriggerを再計算
+  window.addEventListener("resize", () => {
+    ScrollTrigger.refresh();
+  });
 }
